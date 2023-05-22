0ed117dd5a22e9b06223f5bdaf1439c2e7437a23
Sebastian Garcia <jusebastian10@hotmail.com>
2023-05-22 17:29:43 -0500

    basic application structure

---
+++ reto-1/src/App.js
@@ -0,0 +1,119 @@
+import { useEffect, useState } from 'react';
+
+import { Title, TodoInput, TodoList } from './components';
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Hacerme una paja',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Terminar el codigo',
+     completed: false,
+    },
+    {
+      id: 3,
+      title: 'bueno... otra paja',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Cancelar el semestre',
+      completed: false,
+    }
+    
+  ])
+
+  const [activeFilter, setActiveFilter] = useState('all');
+
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false
+    }
+
+    const todoList = [...todos]
+    todoList.push(newTodo);
+
+    setTodos(todoList);
+  }
+
+  const handleSetComplete = (id) => {
+
+    const updatedList = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed}
+      }
+      return todo;
+    })
+
+    setTodos(updatedList);
+  } 
+
+  const handleClearComplete = () => {
+    const updatedList = todos.filter(todo => !todo.completed);
+    setTodos(updatedList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = todos.filter(todo => todo.id !== id);
+    setTodos(updatedList);
+  }
+
+  const showAllTodos = () => {
+    setActiveFilter('all')
+  }
+
+  const showActiveTodos = () => {
+    setActiveFilter('active')
+  }
+
+  const showCompletedTodos = () => {
+    setActiveFilter('completed')
+  }
+  
+  useEffect(() => {
+    if (activeFilter === 'all') {
+      setFilteredTodos(todos);
+    } else if (activeFilter === 'active') {
+        const activeTodos = todos.filter(todo => todo.completed === false);
+        setFilteredTodos(activeTodos);
+    } else if (activeFilter === 'completed') {
+        const completedTodos = todos.filter(todo => todo.completed === true);
+        setFilteredTodos(completedTodos);
+    }
+    
+  },[activeFilter, todos]);
+
+  return (
+    <div className='bg-cyan-200 min-h-screen font-inter h-full text-cyan-400 flex flex-cols flex-wrap items-center justify-center py-20 px-5 '>
+      <div className='container flex flex-col max-w-2xl'>
+        <Title />
+        <TodoInput addTodo={addTodo} />
+        <TodoList
+          activeFilter={activeFilter}
+          todos={filteredTodos}
+          showAllTodos={showAllTodos}
+          showActiveTodos={showActiveTodos}
+          showCompletedTodos={showCompletedTodos}
+          handleSetComplete={handleSetComplete}
+          handleDelete={handleDelete}
+          handleClearComplete={handleClearComplete} />
+      </div>
+
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ reto-1/src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <h1 className="text-4xl font-poppins font-bold tracking-widest text-cyan-50">
+            To-do App
+        </h1>
+    );
+}
+
+export { Title }
